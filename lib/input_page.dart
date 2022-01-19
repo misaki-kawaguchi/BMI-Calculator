ca06912b790506b53096665d8b3c52ed9c4dc1ab
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-19 09:56:27 +0900

    色を個別に設定できるようにする

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,56 +9,67 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body:Column(
-        children: <Widget>[
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(children: <Widget>[
           // 1段目
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
                 ),
               ],
             ),
           ),
           // 2段目
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
           ),
           // 3段目
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
                 ),
               ],
             ),
           ),
-        ]
-      )
-    );
+        ]));
   }
 }
 
 // コンテナ1つ分
 class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  // final：プログラム開始後のある時点で一回だけ初期化される。再代入不可。
+  final Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
