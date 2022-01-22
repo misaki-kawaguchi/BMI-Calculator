e72d1700553c00287589d74610e3545da917f2f5
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-22 18:43:19 +0900

    スライダー作成

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,6 +14,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
 
+  int height = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,14 +81,25 @@ class _InputPageState extends State<InputPage> {
                     textBaseline: TextBaseline.alphabetic,
                     children: <Widget>[
                       Text(
-                        '180',
+                        height.toString(),
                         style: kNumberTextStyle,
                       ),
                       Text(
                         'cm',
                         style: kLabelTextStyle,
                       ),
-                    ]
+                    ]),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
                   ),
                 ],
               ),
