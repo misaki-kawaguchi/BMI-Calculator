e0639a5156c5376de4afdca2b522aa41dd95b8a7
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-22 17:16:53 +0900

    constants.dart作成・移動

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const bottomContainerColour = Color(0xFFEB1555);
+import 'constants.dart';
 
 enum Gender { male, female }
 
@@ -69,6 +65,11 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               colour: activeCardColour,
+              cardChild: Column(
+                children: <Widget>[
+                  Text('Height'),
+                ],
+              ),
             ),
           ),
           // 3段目
