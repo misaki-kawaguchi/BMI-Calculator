b3f62cac5db4356f6b2a0ed1795f43165c483640
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-22 23:48:54 +0900

    getResult作成

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -7,11 +7,24 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
+  double _bmi;
+
   // BMIを計算する
   String calculateBMI() {
     // bmi = 体重 / 高さ(m)の2乗
-    double bmi = weight / pow(height/100, 2);
+    _bmi = weight / pow(height/100, 2);
     // 小数点第一位まで表示する
-    return bmi.toStringAsFixed(1);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  // 結果を表示する
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
   }
 }
