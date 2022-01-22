ba62d4b7a60c2f358de09df936c9e36b9df8a498
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-22 23:10:41 +0900

    RoundIconButtonをround_icon_button.dartに移動

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,6 +6,7 @@ import 'reusable_card.dart';
 import 'constants.dart';
 import 'results_page.dart';
 import 'bottom_button.dart';
+import 'round_icon_button.dart';
 
 enum Gender { male, female }
 
@@ -222,29 +223,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-// アイコンボタン
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
