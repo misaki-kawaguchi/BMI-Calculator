ec5f2c7d302a4bf68e820e882c1852d0fbf96eea
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-22 23:45:02 +0900

    calculateBMI作成

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,7 +1,17 @@
+import 'dart:math';
+
 class CalculatorBrain {
 
   CalculatorBrain({this.height, this.weight});
 
   final int height;
   final int weight;
+
+  // BMIを計算する
+  String calculateBMI() {
+    // bmi = 体重 / 高さ(m)の2乗
+    double bmi = weight / pow(height/100, 2);
+    // 小数点第一位まで表示する
+    return bmi.toStringAsFixed(1);
+  }
 }
